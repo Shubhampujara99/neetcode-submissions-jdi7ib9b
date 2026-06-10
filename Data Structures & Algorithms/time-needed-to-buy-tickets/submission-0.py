981586d568b084166c4aadce1a1e3babb708ede9
Shubhampujara99 <shubhampujara99@gmail.com>
2026-06-10 10:18:52 +0530

    Add: time-needed-to-buy-tickets - submission-0

---
+++ Data Structures & Algorithms/time-needed-to-buy-tickets/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        ans = 0
+        for i in range(len(tickets)):
+            if(i<=k):
+                ans += min(tickets[i],tickets[k])
+            else:
+                ans +=min(tickets[k]-1,tickets[i])
+        return ans
